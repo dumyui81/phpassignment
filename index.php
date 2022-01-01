cc8d58f4c54b8d726f990ae24f56c3cae3d0f537
dumyui81 <96933448+dumyui81@users.noreply.github.com>
2022-01-01 19:41:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+
+$url = "https://c.xkcd.com/random/comic/";
+$ch  = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$a = curl_exec($ch);
+$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+$str  = file_get_contents($url.'info.0.json');
+$json = json_decode($str, true);
+$imageTitle = $json['title'];
+$imageUrl = $json['img'];
+$imageAlt = $json['alt'];
+$imageFile = file_get_contents($imageUrl);
+
+
 include('smtp/PHPMailerAutoload.php');
 $html=$message = '
 <html>
