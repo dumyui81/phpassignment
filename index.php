7ddf22a773884a21b42770df20f4273275843836
dumyui81 <96933448+dumyui81@users.noreply.github.com>
2022-01-02 11:42:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 include('Mailin.php');
 use Sendinblue\Mailin;
-$api_key = '**********';
+$api_key = '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';
 
-$from_email = '*****@gmail.com';
+$from_email = 'dumyui81@gmail.com';
 $from_name = 'Name of Sender';
 
-$to_email = '******@gmail.com';
+$to_email = 'dumyui81@gmail.com';
 $to_name = 'Name of Receiver';
 $subject = 'This is the subject';
 $message = '<h2>Heading 2</h2><p>Here goes the paragraph blah blah blah</p>';
